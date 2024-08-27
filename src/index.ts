8e8eeb037a7ee35b9edad5f475ffe43ee75340cf
ndhung <nguyenduyhung04092004@gmail.com>
2024-08-27 08:13:51 +0700

    .

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.APP_PORT);
+const HOST = `${process.env.APP_HOST}`;
 
 app.use(express.static(path.join(__dirname, '../')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,11 +21,11 @@ app.use(morgan('combined'))
 
 app.use(router);
 
-app.listen(PORT, async () => {
+app.listen(PORT, HOST,async () => {
     try {
         setUpEmitter();
         await init();
-        console.log(`Run server successfully!`);
+        console.log(`Server is listening on http://${HOST}:${PORT}`);
     } catch (error) {
         console.log(error);
     }
