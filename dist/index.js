732788171e5024708078858edbe7f4c83cf285e8
ndhung <nguyenduyhung04092004@gmail.com>
2024-09-04 07:35:16 +0700

    .

--- dist/index.js
+++ dist/index.js
@@ -20,26 +20,37 @@ const PORT = Number(process.env.APP_PORT);
 const HOST = `${process.env.APP_HOST}`;
 app.use((0, cors_1.default)({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(express_1.default.static(path_1.default.join(__dirname, '../')));
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(body_parser_1.default.json());
 app.use((0, morgan_1.default)('combined'));
 app.use("/api", routers_1.default);
-console.log(__dirname);
 const sslOptions = {
     key: fs_1.default.readFileSync(path_1.default.resolve(__dirname, 'keys/server.key')),
     cert: fs_1.default.readFileSync(path_1.default.resolve(__dirname, 'keys/server.crt')),
 };
-https_1.default.createServer(sslOptions, app).listen(PORT, HOST, async () => {
-    try {
-        (0, emit_1.setUpEmitter)();
-        await (0, init_1.default)();
-        console.log(`Server is listening on https://${HOST}:${PORT}`);
-    }
-    catch (error) {
-        console.log(error);
-    }
+const startServer = () => {
+    https_1.default.createServer(sslOptions, app).listen(PORT, HOST, async () => {
+        try {
+            (0, emit_1.setUpEmitter)();
+            await (0, init_1.default)();
+            console.log(`Server is listening on https://${HOST}:${PORT}`);
+        }
+        catch (error) {
+            console.log('Lỗi khi khởi tạo máy chủ:', error);
+            process.exit(1);
+        }
+    });
+};
+process.on('uncaughtException', (err) => {
+    console.log('Ngoại lệ không được bắt:', err);
+    startServer();
 });
+process.on('unhandledRejection', (err) => {
+    console.log('Từ chối không được xử lý:', err);
+    startServer();
+});
+startServer();
 exports.default = app;
